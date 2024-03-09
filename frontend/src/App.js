bfa0cc0374a21b9dfccd3613a9f726d6c6b51140
manish singh rawat mani1444 <manishpb1433@gmail.com>
2024-03-10 00:46:42 +0530

    WIP

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,32 @@
-
-import Header from '../components/UIC/Header';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './App.css';
+
+import AddExperience from './components/pages/AddExperience';
+import ExperienceDetails from './components/pages/ExperienceDetails';
+import Home from './components/pages/Home';
+import Header from './components/UIC/Header';
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home></Home>
+  },
+  {
+    path: '/addexperience',
+    element: <AddExperience></AddExperience>
+  },
+  {
+    path: '/experiencedetails/:id',
+    element: <ExperienceDetails></ExperienceDetails>
+  }
+])
 
 function App() {
   return (
     <div>
-      <Header
-      ></Header>
-
+      <Header></Header>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
