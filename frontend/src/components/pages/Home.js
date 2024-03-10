b55dbf684a301ef219056974060b1273decb489c
manish singh rawat mani1444 <manishpb1433@gmail.com>
2024-03-10 18:52:31 +0530

    wip

--- frontend/src/components/pages/Home.js
+++ frontend/src/components/pages/Home.js
@@ -5,44 +5,44 @@ import FeaturedCard from "../UIC/FeaturedCard";
 import { baseUrl } from "../UIC/url";
 
 export default function Home() {
-  const [data, setData] = useState();
-  useEffect(() => {
-    axios.get(`${baseUrl}/trip/`).then((res) => setData(res.data));
-  }, []);
-  if (data) {
-    return (
-      <div style={{ margin: "2%" }}>
-        {data.map((e) => {
-          if (e.featured) {
-            return (
-              <FeaturedCard
-                title={e.tripName}
-                tripType={e.tripType}
-                description={e.shortDescription}
-                id={e._id}
-              ></FeaturedCard>
-            );
-          } else {
-            return null;
-          }
-        })}
-        {data.map((e) => {
-          if (!e.featured) {
-            return (
-              <Card
-                title={e.tripName}
-                tripType={e.tripType}
-                description={e.shortDescription}
-                id={e._id}
-              ></Card>
-            );
-          } else {
-            return null;
-          }
-        })}
-      </div>
-    );
-  } else {
-    return <>Loading...</>;
-  }
+    const [data, setData] = useState();
+    useEffect(() => {
+        axios.get(`${baseUrl}/trip/`).then((res) => setData(res.data));
+    }, []);
+    if (data) {
+        return (
+            <div style={{ margin: "2%" }}>
+                {data.map((e) => {
+                    if (e.featured) {
+                        return (
+                            <FeaturedCard
+                                title={e.tripName}
+                                tripType={e.tripType}
+                                description={e.shortDescription}
+                                id={e._id}
+                            ></FeaturedCard>
+                        );
+                    } else {
+                        return null;
+                    }
+                })}
+                {data.map((e) => {
+                    if (!e.featured) {
+                        return (
+                            <Card
+                                title={e.tripName}
+                                tripType={e.tripType}
+                                description={e.shortDescription}
+                                id={e._id}
+                            ></Card>
+                        );
+                    } else {
+                        return null;
+                    }
+                })}
+            </div>
+        );
+    } else {
+        return <>Loading...</>;
+    }
 }
